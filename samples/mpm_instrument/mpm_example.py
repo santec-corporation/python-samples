cd4cd45f79c3869e97df298cc362e4a4ff515332
rpj17-santec <rakshith.pj@santec.com>
2025-04-23 14:47:25 +0900

    Update mpm_example.py

--- samples/mpm_instrument/mpm_example.py
+++ samples/mpm_instrument/mpm_example.py
@@ -5,10 +5,13 @@ import pyvisa
 rm = pyvisa.ResourceManager()
 
 # Open a connection to the MPM instrument via GPIB
-instrument = rm.open_resource('GPIB0::11::INSTR')        # Replace with your instrument's GPIB address
+instrument = rm.open_resource('GPIB0::16::INSTR')        # Replace with your instrument's GPIB address
 
 # Create a connection of the MPM class
 mpm = MPM(instrument)
 
 # Print the instrument identification
 print(mpm.get_idn())
+
+# Print logging data length
+print(len(mpm.get_logging_data(0,1)))
