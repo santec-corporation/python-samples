b05a6c5a67e6b2eb2808dcbeb1c59b63ae2c59ef
rpj17-santec <rakshith.pj@santec.com>
2025-04-29 14:17:53 +0900

    Rename fetch_wavelength_gpib.py to get_wavelength_gpib.py

--- samples/tsl_instrument/get_wavelength_gpib.py
+++ samples/tsl_instrument/get_wavelength_gpib.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Script to retrieve TSL wavelength data via LAN communication.
+Script to retrieve TSL wavelength data via GPIB communication.
 """
 
 import math
@@ -22,7 +22,12 @@ expected_size = count * 4 + (2 + 1 + int(math.log10(count)))
 
 with tqdm(total=expected_size, unit='B', unit_scale=True) as progress:
     response = tsl.query_binary_values(':READout:DATa?',
+                                       datatype='i',
                                        chunk_size=expected_size,
                                        monitoring_interface=progress)
 
+print(response)
 print(len(response))
+
+rounded = [float(f"{num / 10000:.4f}") for num in response]
+print(rounded)
