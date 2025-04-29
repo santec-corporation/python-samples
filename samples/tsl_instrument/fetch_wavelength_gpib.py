5ab0955f8358700a24a4e5ce1fc4ff024c72ffe5
rpj17-santec <rakshith.pj@santec.com>
2025-04-29 12:50:32 +0900

    Create fetch_wavelength_gpib.py

---
+++ samples/tsl_instrument/fetch_wavelength_gpib.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Script to retrieve TSL power data via LAN communication.
+"""
+
+import math
+import pyvisa
+from tqdm import tqdm
+
+rm = pyvisa.ResourceManager()
+
+gpib_resource = "GPIB0::1::INSTR"
+tsl = rm.open_resource(gpib_resource)
+
+idn = tsl.query('*IDN?')
+print("IDN: ", idn)
+
+count = int(tsl.query('READout:POINts?'))
+print("LOGGING POINTS: ", count)
+
+expected_size = count * 4 + (2 + 1 + int(math.log10(count)))
+
+with tqdm(total=expected_size, unit='B', unit_scale=True) as progress:
+    response = tsl.query_binary_values(':READout:DATa?',
+                                       chunk_size=expected_size,
+                                       monitoring_interface=progress)
+
+print(len(response))
